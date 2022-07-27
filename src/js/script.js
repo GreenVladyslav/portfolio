a12eb465253235335ae95d6fed09ff0799094876
Vladyslav <jrlimueno@gmail.com>
2022-07-27 12:33:28 +0300

    menu script

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const trigger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+    const close = document.querySelector('.menu__close');
+
+    trigger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    close.addEventListener('click', () => {
+            menu.classList.remove('active');
+    });
+
+    menu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('menu__overlay')) {
+            menu.classList.remove('active');
+        }
+    });
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            menu.classList.remove('active');
+        }
+    });
+});
